2b730d8e6b32fc9469e228dec6a0593ebac394ac
Tomohiko Hiraki <hirakitomohiko@gmail.com>
2023-04-23 01:45:05 +0900

    add categories

--- frontend/src/gql/gql.ts
+++ frontend/src/gql/gql.ts
@@ -14,8 +14,9 @@ import type { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-
  */
 const documents = {
     "query queryBlogSettings {\n  blogSetting {\n    data {\n      attributes {\n        title\n        seo {\n          ...Seo\n        }\n      }\n    }\n  }\n}": types.QueryBlogSettingsDocument,
-    "query queryPostBySlug($slug: String) {\n  posts(filters: {slug: {eq: $slug}}) {\n    data {\n      attributes {\n        title\n        content\n        slug\n        seo {\n          ...Seo\n        }\n      }\n    }\n  }\n}": types.QueryPostBySlugDocument,
-    "query queryPosts {\n  posts(sort: \"publishedAt:desc\") {\n    data {\n      attributes {\n        title\n        content\n        slug\n        seo {\n          ...Seo\n        }\n      }\n    }\n  }\n}": types.QueryPostsDocument,
+    "fragment Categories on CategoryRelationResponseCollection {\n  data {\n    attributes {\n      name\n      slug\n      posts {\n        data {\n          attributes {\n            slug\n          }\n        }\n      }\n    }\n  }\n}": types.CategoriesFragmentDoc,
+    "query queryCategories {\n  categories(sort: \"updatedAt:desc\") {\n    data {\n      attributes {\n        name\n        slug\n        posts {\n          data {\n            attributes {\n              title\n              content\n              slug\n              categories {\n                ...Categories\n              }\n            }\n          }\n        }\n      }\n    }\n  }\n}": types.QueryCategoriesDocument,
+    "query queryPosts {\n  posts(sort: \"publishedAt:desc\") {\n    data {\n      attributes {\n        title\n        content\n        slug\n        categories {\n          ...Categories\n        }\n        seo {\n          ...Seo\n        }\n      }\n    }\n  }\n}": types.QueryPostsDocument,
     "fragment Seo on ComponentSharedSeo {\n  metaTitle\n  metaImage {\n    data {\n      attributes {\n        url\n        mime\n      }\n    }\n  }\n  metaSocial {\n    socialNetwork\n    title\n    description\n  }\n  metaRobots\n  metaViewport\n  metaDescription\n  keywords\n  structuredData\n  canonicalURL\n}": types.SeoFragmentDoc,
 };
 
@@ -40,11 +41,15 @@ export function graphql(source: "query queryBlogSettings {\n  blogSetting {\n   
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(source: "query queryPostBySlug($slug: String) {\n  posts(filters: {slug: {eq: $slug}}) {\n    data {\n      attributes {\n        title\n        content\n        slug\n        seo {\n          ...Seo\n        }\n      }\n    }\n  }\n}"): (typeof documents)["query queryPostBySlug($slug: String) {\n  posts(filters: {slug: {eq: $slug}}) {\n    data {\n      attributes {\n        title\n        content\n        slug\n        seo {\n          ...Seo\n        }\n      }\n    }\n  }\n}"];
+export function graphql(source: "fragment Categories on CategoryRelationResponseCollection {\n  data {\n    attributes {\n      name\n      slug\n      posts {\n        data {\n          attributes {\n            slug\n          }\n        }\n      }\n    }\n  }\n}"): (typeof documents)["fragment Categories on CategoryRelationResponseCollection {\n  data {\n    attributes {\n      name\n      slug\n      posts {\n        data {\n          attributes {\n            slug\n          }\n        }\n      }\n    }\n  }\n}"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(source: "query queryPosts {\n  posts(sort: \"publishedAt:desc\") {\n    data {\n      attributes {\n        title\n        content\n        slug\n        seo {\n          ...Seo\n        }\n      }\n    }\n  }\n}"): (typeof documents)["query queryPosts {\n  posts(sort: \"publishedAt:desc\") {\n    data {\n      attributes {\n        title\n        content\n        slug\n        seo {\n          ...Seo\n        }\n      }\n    }\n  }\n}"];
+export function graphql(source: "query queryCategories {\n  categories(sort: \"updatedAt:desc\") {\n    data {\n      attributes {\n        name\n        slug\n        posts {\n          data {\n            attributes {\n              title\n              content\n              slug\n              categories {\n                ...Categories\n              }\n            }\n          }\n        }\n      }\n    }\n  }\n}"): (typeof documents)["query queryCategories {\n  categories(sort: \"updatedAt:desc\") {\n    data {\n      attributes {\n        name\n        slug\n        posts {\n          data {\n            attributes {\n              title\n              content\n              slug\n              categories {\n                ...Categories\n              }\n            }\n          }\n        }\n      }\n    }\n  }\n}"];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "query queryPosts {\n  posts(sort: \"publishedAt:desc\") {\n    data {\n      attributes {\n        title\n        content\n        slug\n        categories {\n          ...Categories\n        }\n        seo {\n          ...Seo\n        }\n      }\n    }\n  }\n}"): (typeof documents)["query queryPosts {\n  posts(sort: \"publishedAt:desc\") {\n    data {\n      attributes {\n        title\n        content\n        slug\n        categories {\n          ...Categories\n        }\n        seo {\n          ...Seo\n        }\n      }\n    }\n  }\n}"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
