bc167469f8a566a868ad245ad9196498b2df2117
Tomohiko Hiraki <hirakitomohiko@gmail.com>
2023-09-24 18:12:35 +0900

    remove compress

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config"
 import svelte from "@astrojs/svelte"
 import prefetch from "@astrojs/prefetch"
 import tailwind from "@astrojs/tailwind"
-import compress from "astro-compress"
 import robotsTxt from "astro-robots-txt"
 import sitemap from "@astrojs/sitemap"
 
@@ -15,7 +14,6 @@ export default defineConfig({
     svelte(),
     prefetch(),
     tailwind(),
-    compress(),
     sitemap(),
     robotsTxt(),
     partytown({
