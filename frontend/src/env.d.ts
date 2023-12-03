834f941c2c83024cdfab1fa6e6c2f096300e21e4
Tomohiko Hiraki <hirakitomohiko@gmail.com>
2023-12-04 00:31:24 +0900

    fix

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -1,3 +1,19 @@
 /// <reference types="astro/client" />
 
+type KVNamespace = import("@cloudflare/workers-types/experimental").KVNamespace
+type D1Database = import("@cloudflare/workers-types/experimental").D1Database
+type ENV = {
+  SERVER_URL: string
+  KV_BINDING: KVNamespace
+  DB: D1Database
+}
+
 type Runtime = import("@astrojs/cloudflare").AdvancedRuntime<ENV>
+declare namespace App {
+  interface Locals extends Runtime {
+    user: {
+      name: string
+      surname: string
+    }
+  }
+}
